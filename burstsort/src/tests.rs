0d42f6309adf3c7a1467e381f3f1ffb4b6c02ad4
noah <nomaxx117@gmail.com>
2021-08-04 11:19:24 -0500

    Fixed test issue

--- burstsort/src/tests.rs
+++ burstsort/src/tests.rs
@@ -6,6 +6,25 @@ fn check_sort_string(mut data: Vec<String>) {
         burst_limit: 8,
         initial_capacity: 4,
         classes: 256,
+        hint_long: false
+    };
+
+    let mut expected = data.clone();
+
+    burstsort(&mut data, &config);
+
+    expected.sort();
+
+    assert_eq!(expected, data);
+}
+
+#[quickcheck]
+fn check_sort_string_long(mut data: Vec<String>) {
+    let config = BurstConfig {
+        burst_limit: 8,
+        initial_capacity: 4,
+        classes: 256,
+        hint_long: true
     };
 
     let mut expected = data.clone();
